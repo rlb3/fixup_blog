8197c75d184260a2487f26d0037a4733c038719e
Robert Boone <robert@jrlbconsulting.com>
2013-02-24 13:42:52 -0600

    fix spelling mistake

--- spec/post_file_spec.rb
+++ spec/post_file_spec.rb
@@ -4,8 +4,9 @@ require 'post_file'
 require File.expand_path(File.join(File.dirname(__FILE__), "spec_helper.rb"))
 
 describe PostFile do
-  describe '.path' do
-    let(:path) { File.dirname(__FILE__) + '/fixture/2012-10-20-all-my-old-posts.markdown' }
+
+  context 'with a correctly formatted file' do
+    let(:path) { File.dirname(__FILE__) + '/fixtures/2012-10-20-all-my-old-posts.markdown' }
     subject { PostFile.new(path) }
     
     it { subject.path.should == path }
