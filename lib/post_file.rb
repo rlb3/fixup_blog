f8a8b830e6a4d88e8c6a6975e6c6b709800546a5
Robert Boone <robert@jrlbconsulting.com>
2013-02-24 13:44:13 -0600

    Parse the file into headers and body

--- lib/post_file.rb
+++ lib/post_file.rb
@@ -1,7 +1,24 @@
+require 'yaml'
+
 class PostFile
   attr_accessor :path
+  attr_accessor :headers
+  attr_accessor :body
 
   def initialize(path)
     @path = path
   end
+
+  def parse
+    text    = IO.read(self.path)
+    matcher = %r{---(.*?)---(.*)}m.match(text)
+    yaml    = matcher[1]
+    body    = matcher[2]
+
+    @headers = YAML.load(yaml)
+    @body    = body
+
+    true if @headers && @body
+  end
+
 end
--- spec/post_file_spec.rb
+++ spec/post_file_spec.rb
@@ -1,14 +1,19 @@
 $:.unshift(File.dirname(__FILE__) + '/../lib')
-require 'post_file'
-
 require File.expand_path(File.join(File.dirname(__FILE__), "spec_helper.rb"))
+require 'post_file'
 
 describe PostFile do
 
   context 'with a correctly formatted file' do
     let(:path) { File.dirname(__FILE__) + '/fixtures/2012-10-20-all-my-old-posts.markdown' }
     subject { PostFile.new(path) }
-    
-    it { subject.path.should == path }
+
+    describe '.path' do
+      it { subject.path.should == path }
+    end
+
+    describe '.parse' do
+      it { subject.parse.should be_true }
+    end
   end
 end
